12cf5bda82decbb92193c9e32967443c60324151
StageGuard <1355416608@qq.com>
2023-10-31 09:51:19 +0800

    reimplement ch3 & ch4

--- os/src/task/mod.rs
+++ os/src/task/mod.rs
@@ -35,6 +35,9 @@ pub use processor::{
     current_task, current_trap_cx, current_user_token, run_tasks, schedule, take_current_task,
     Processor,
 };
+use crate::config::{MAX_SYSCALL_NUM, PAGE_SIZE};
+use crate::mm::MapPermission;
+
 /// Suspend the current 'Running' task and run the next task in task list.
 pub fn suspend_current_and_run_next() {
     // There must be an application running.
@@ -99,6 +102,67 @@ pub fn exit_current_and_run_next(exit_code: i32) {
     // we do not have to save task context
     let mut _unused = TaskContext::zero_init();
     schedule(&mut _unused as *mut _);
+}
+
+/// Apply for a new virtual memory area to current task
+pub fn mmap_memory_area_for_current(start_va: usize, len: usize, prot: usize) -> bool {
+    // start is not aligned to PAGE_SIZE
+    if start_va % PAGE_SIZE != 0 { return false }
+
+    let mut perm = MapPermission::U;
+
+    if prot & !0x7 != 0 { return false }
+    if prot & 0x7 == 0 { return false }
+
+    if prot & 0b1 == 1 { perm |= MapPermission::R; }
+    if prot & 0b10 == 2 { perm |= MapPermission::W; }
+    if prot & 0b100 == 4 { perm |= MapPermission::X; }
+
+    let task = current_task().unwrap();
+    let mut inner = task.inner_exclusive_access();
+    let mem_set = &mut inner.memory_set;
+
+    let start = (start_va / PAGE_SIZE) * PAGE_SIZE;
+    let mut end = (start_va + len) / PAGE_SIZE * PAGE_SIZE;
+    if (start + len) % PAGE_SIZE > 0 { end += PAGE_SIZE }
+
+    if mem_set.is_conflict(start..end) { return false }
+
+    mem_set.insert_framed_area(start.into(), end.into(), perm);
+    true
+}
+
+/// Apply for a new virtual memory area to current task
+pub fn munmap_memory_area_for_current(start_va: usize, len: usize) -> bool {
+    // start is not aligned to PAGE_SIZE
+    if start_va % PAGE_SIZE != 0 { return false }
+
+    let task = current_task().unwrap();
+    let mut inner = task.inner_exclusive_access();
+    let mem_set = &mut inner.memory_set;
+
+    let start = (start_va / PAGE_SIZE) * PAGE_SIZE;
+    let mut end = (start_va + len) / PAGE_SIZE * PAGE_SIZE;
+    if (start + len) % PAGE_SIZE > 0 { end += PAGE_SIZE }
+
+    mem_set.recycle_map_area(start.into(), end.into())
+}
+
+/// Increment syscall stat to current running task
+pub fn increment_syscall_stat(syscall_id: usize) {
+    let task = current_task().unwrap();
+    let mut inner = task.inner_exclusive_access();
+
+    if inner.task_status != TaskStatus::Running { return }
+    inner.syscall_statistics[syscall_id] += 1;
+}
+/// Get syscall stat of current running task
+pub fn get_current_task_syscall_stat() -> Option<(TaskStatus, [u32; MAX_SYSCALL_NUM])> {
+    let task = current_task().unwrap();
+    let inner = task.inner_exclusive_access();
+
+    Some((inner.task_status.clone(),
+          inner.syscall_statistics.clone()))
 }
 
 lazy_static! {
--- os/src/task/task.rs
+++ os/src/task/task.rs
@@ -1,7 +1,7 @@
 //! Types related to task management & Functions for completely changing TCB
 use super::TaskContext;
 use super::{kstack_alloc, pid_alloc, KernelStack, PidHandle};
-use crate::config::TRAP_CONTEXT_BASE;
+use crate::config::{MAX_SYSCALL_NUM, TRAP_CONTEXT_BASE};
 use crate::mm::{MemorySet, PhysPageNum, VirtAddr, KERNEL_SPACE};
 use crate::sync::UPSafeCell;
 use crate::trap::{trap_handler, TrapContext};
@@ -68,6 +68,9 @@ pub struct TaskControlBlockInner {
 
     /// Program break
     pub program_brk: usize,
+
+    /// The task syscall statistics
+    pub syscall_statistics: [u32; MAX_SYSCALL_NUM],
 }
 
 impl TaskControlBlockInner {
@@ -118,6 +121,7 @@ impl TaskControlBlock {
                     exit_code: 0,
                     heap_bottom: user_sp,
                     program_brk: user_sp,
+                    syscall_statistics: [0; MAX_SYSCALL_NUM]
                 })
             },
         };
@@ -191,6 +195,7 @@ impl TaskControlBlock {
                     exit_code: 0,
                     heap_bottom: parent_inner.heap_bottom,
                     program_brk: parent_inner.program_brk,
+                    syscall_statistics: [0; MAX_SYSCALL_NUM]
                 })
             },
         });
@@ -236,6 +241,7 @@ impl TaskControlBlock {
                     exit_code: 0,
                     heap_bottom: user_sp,
                     program_brk: user_sp,
+                    syscall_statistics: [0; MAX_SYSCALL_NUM]
                 })
             },
         });
